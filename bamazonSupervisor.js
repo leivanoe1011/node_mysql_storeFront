0fa875344559a85f904445dea24bd25e41445df2
Noe Leiva <leiva.noe.86@gmail.com>
2020-06-28 23:31:04 -0500

    function view product by department has been created

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -6,9 +6,37 @@
 
     var appFunctions = ["Product By Department", "Create New Department"];
 
+    var connection = mySql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "BAMAZON_DB"
+    });
 
     function viewProductByDepartment(){
 
+        var query = "SELECT " +
+                "A.DEPARTMENT_ID " +
+                ",A.DEPARTMENT_NAME " +
+                ",A.OVERHEAD_COST " +
+                ",SUM(COALESCE(B.PRODUCT_SALES,0)) PRODUCT_SALES " +
+                ",(SUM(COALESCE(B.PRODUCT_SALES,0)) - OVERHEAD_COST ) TOTAL_PROFIT " +
+                "FROM DEPARTMENTS A " +
+            "JOIN PRODUCTS B " +
+            "ON B.DEPARTMENT_NAME = A.DEPARTMENT_NAME " +
+            "GROUP BY A.DEPARTMENT_NAME, A.DEPARTMENT_ID, B.PRODUCT_SALES, A.OVERHEAD_COST;";
+
+        connection.query(query, function(err,data){
+            if(err){
+                connection.end();
+                throw err;
+            }
+
+            for(var i = 0; i < data.length; i++){
+                
+            }
+        })
     }
 
 
